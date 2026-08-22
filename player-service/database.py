bebd51fa0255a3445c436da9f585bfa19c62b23a
Nikita Benny <nikita.bennyj@gmail.com>
2026-08-22 01:28:57 -0400

    dockerized player-service container

--- player-service/database.py
+++ player-service/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+import os
 
-DATABASE_URL = 'postgresql+psycopg2://pitchfolio:pitchfolio@localhost:5432/player_service'
+DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
